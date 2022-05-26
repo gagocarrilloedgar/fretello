5a76a72d34c051186274fe36a98a6110756e8973
Edgar <edgargc.upc@gmail.com>
2022-05-26 18:22:44 +0200

    feat: add request validation using express-validator and custom catchasync function

---
+++ src/shared/middlewares/validateRequest.ts
@@ -0,0 +1,12 @@
+import { Response, Request, NextFunction } from 'express'
+import { validationResult } from 'express-validator'
+import { catchAsync } from 'shared/utils'
+
+export const validateRequest = () =>
+  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req)
+
+    if (errors.isEmpty()) return next()
+
+    res.status(400).json({ errors: errors.array() })
+  })
